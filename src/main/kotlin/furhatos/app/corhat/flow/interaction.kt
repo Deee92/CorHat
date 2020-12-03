6799148c99e1db7ba044f10c5fc5f9420801f4c4
Deepika Tiwari <deepikatiwari92@gmail.com>
2020-12-03 14:01:42 +0100

    Get user query

--- src/main/kotlin/furhatos/app/corhat/flow/interaction.kt
+++ src/main/kotlin/furhatos/app/corhat/flow/interaction.kt
@@ -1,19 +1,47 @@
 package furhatos.app.corhat.flow
 
+import furhatos.app.corhat.nlu.GetGeneralInfoIntent
+import furhatos.app.corhat.nlu.GiveSymptomsIntent
 import furhatos.nlu.common.*
 import furhatos.flow.kotlin.*
 
 val Start : State = state(Interaction) {
 
     onEntry {
-        furhat.ask("Hi there. Do you have questions about COVID-19?")
+        furhat.ask("Hi there, my name is CorHat. Do you have questions about COVID-19?")
     }
 
     onResponse<Yes>{
-        furhat.say("You came to the right place! How can I help you today?")
+        furhat.say("You came to the right place!");
+        goto(GetQuery)
     }
 
-    onResponse<No>{
+    onResponse<No> {
         furhat.say("Okay, have a nice day!")
+        goto(Idle)
+    }
+}
+
+// Placeholder answers :)
+val GetQuery = state(parent = Start) {
+    onEntry {
+        furhat.ask("How can I help you today?")
+    }
+
+    onResponse<GetGeneralInfoIntent>{
+        furhat.say("COVID-19 or the novel Coronavirus is a bad disease");
+        goto(EndInteraction)
+    }
+
+    onResponse<GiveSymptomsIntent>{
+        furhat.say("Sorry to hear that!");
+        goto(EndInteraction)
+    }
+}
+
+val EndInteraction = state {
+    onEntry {
+        furhat.say("Thanks for your time. Goodbye!")
+        goto(Idle)
     }
 }
